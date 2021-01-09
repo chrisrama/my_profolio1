a2240cb225884ce495ae4059b712fbc9104f1002
Chris Rama <chrisramazani123@gmail.com>
2021-01-09 11:02:51 +0000

    noe change

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const nav = document.querySelector(".elements");
-const button = document.querySelector(".rows");
-const spans = document.querySelectorAll(".span");
-
-function toggler() {
-	nav.classList.toggle("sidebar");
-	spans[0].classList.toggle("line-0");
-	spans[1].classList.toggle("line-1");
-	spans[2].classList.toggle("line-2");
-}
-
-button.addEventListener("click", toggler);
+var myFunction = () => {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+    console.log("hello world");
+  }
+  console.log("hello world");
